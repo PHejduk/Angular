4be895d8596d734d9289513124f03449a56b9921
Piotr <pah6@op.pl>
2023-07-22 01:41:09 +0200

    commit 3

--- src/app/components/task-list.component.ts
+++ src/app/components/task-list.component.ts
@@ -6,6 +6,7 @@ import { UserService } from '../services/user.service';
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.scss']
 })
 export class TaskListComponent {
   tasks: Task[];
